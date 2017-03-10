08ca76255218fc811873d741a529a8ac55ac7ec9
Austin Lubetkin <bocaaust@gmail.com>
2017-03-10 15:44:01 -0500

    Google Sign In Pods

---
+++ Google Sign In Test/Pods/Target Support Files/Pods-Google Sign In TestTests/Pods-Google Sign In TestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Google_Sign_In_TestTests : NSObject
+@end
+@implementation PodsDummy_Pods_Google_Sign_In_TestTests
+@end
---
+++ Google Sign In Test/Pods/Target Support Files/Pods-Google Sign In TestUITests/Pods-Google Sign In TestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Google_Sign_In_TestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Google_Sign_In_TestUITests
+@end
